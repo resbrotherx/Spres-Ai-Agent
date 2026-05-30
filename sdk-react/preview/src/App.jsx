dfe11437ef47e7690b12a2e951ca900d0ea9ef6a
Resbrotherx <francisdaniel140@gmail.com>
2026-05-30 16:31:49 +0100

    updated

--- sdk-react/preview/src/App.jsx
+++ sdk-react/preview/src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrainboxReactSDK, ChatPanel, ChatWidget } from '../../dist/index.js';
+import { BrainboxReactSDK, ChatPanel, ChatWidget } from 'spres-react';
 
 const sdk = new BrainboxReactSDK(
   'https://demo.brainbox.example',
